b79f1f31aae3f7fce2690d7f06ef25e0618c1ffd
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-07-11 21:54:10 +0530

    2024.07.11

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
+import ProfileInfo from "../cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-        <h2 className="text-x1 font-medium text-black py-2 ">Notes</h2>
-    </div>
-  )
-}
+      <h2 className="text-x1 font-medium text-black py-2 ">DK - Notes</h2>
 
-export default Navbar
+      <SearchBar />
+      <ProfileInfo onLogout={onLogout} />
+    </div>
+  );
+};
+
+export default Navbar;
