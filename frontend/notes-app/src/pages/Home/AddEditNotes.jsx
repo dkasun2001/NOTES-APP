0f1309ce14e8c34c9b883dc84cf5097dcda11ee3
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-07-17 21:30:10 +0530

    2024.07.14

---
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import TagInput from "../../components/Input/TagInput";
+
+const AddEditNotes = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2">
+        <label className="input-lable">TITLE</label>
+        <input
+          type="text"
+          className="text-2xl text-slate-950 outline-none bg-slate-100"
+          placeholder="Go to gym at 5"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="input-lable">CONTENT</label>
+        <textarea
+          type="text"
+          className="text-sm text-slate-950 outline-none bg-slate-100 p-2 rounded "
+          placeholder="Content"
+          rows={10}
+          value={content}
+          onChange={({ target }) => setContent(target.value)}
+        />
+      </div>
+
+      <div className="mt-3">
+        <label className="input-lable">TAGS</label>
+        <TagInput tags={tags} setTags={setTags} />
+      </div>
+
+      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+        ADD
+      </button>
+    </div>
+  );
+};
+
+export default AddEditNotes;
