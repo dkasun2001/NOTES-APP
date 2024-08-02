7a2872d9785a5c0fae3aefacea260cd84bc1af19
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-08-03 01:14:39 +0530

    2024.08.02

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.connectionString);
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require("jsonwebtoken")
+const {} = require("./utilities")
 
 app.use(express.json());
 
@@ -13,6 +23,15 @@ app.use(
 app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
+
+// Create Account
+app.post("/create-account",async(req,res)=>{
+
+  const{fillName,emaill}
+
+}
+
+)
 
 app.listen(8000);
 
